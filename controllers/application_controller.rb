39b4e25646bf6bc22d370987fe889e018781d9d1
Brian Marshall <bmarsha72@gmail.com>
2016-03-28 13:49:07 -0500

    params for business

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < Sinatra::Base
 
-  enable :sessions
 
   set :views, File.expand_path("../../views", __FILE__)
   set :public_folder, File.dirname(__FILE__) + '/../public'
