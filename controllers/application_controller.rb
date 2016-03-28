5e68a00b722f4d2cde070738fa0d62dd72628788
Brian Marshall <bmarsha72@gmail.com>
2016-03-28 13:26:15 -0500

    application controller with erb :index route

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < Sinatra::Base
 
+  enable :sessions
+
   set :views, File.expand_path("../../views", __FILE__)
   set :public_folder, File.dirname(__FILE__) + '/../public'
 
   get '/' do
-    erb :homepage
+    erb :index
   end
-
 end
