c520d0e6c7583a057ac50000898bac304bf3c64f
cam iverson <camiver@gmail.com>
2016-03-29 15:36:16 -0500

    updating code with JS and some backend for searching zip codes

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < Sinatra::Base
 
+  @zip_code = ""
   enable :sessions
 
   set :views, File.expand_path('../../views', __FILE__)
@@ -20,6 +21,11 @@ class ApplicationController < Sinatra::Base
   get '/logout' do
     session[:logged_in] = false
     redirect '/'
+  end
+
+  post '/search' do
+    @zip_code = params[:zipcode]
+    erb :results
   end
 
 end
